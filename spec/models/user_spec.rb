a6108d1b3fb6fa05365a5934c022e18566f976bb
salin paul <salin.paul@geckolyst.com>
2024-04-09 21:21:07 +0530

    testing expect

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it 'has a valid factory' do
+    expect(user).to be_valid 
+    # expect(user.valid?).to eq(true)
+    # expect('').to be_empty
+    # expect({a: 1}).to be_a(Hash)
+  end
 end
