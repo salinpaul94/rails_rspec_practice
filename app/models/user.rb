0162a5105e6d806039289993868c7e6ffb9cb7b2
salin paul <salin.paul@geckolyst.com>
2024-04-09 21:42:58 +0530

    adding spec for user, user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name,
+             presence: true,
+             uniqueness: true
+
+	validates :email,
+	           presence: true,
+						 uniqueness: true,
+						 format: {with: /\A\S+@\S+\z/}
 end
