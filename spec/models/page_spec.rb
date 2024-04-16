eec56e025f73132e42d34e2af500e592d9bad26b
salinpaul valooran <salin.paul@geckolyst.com>
2024-04-16 23:32:43 +0530

    make slug from given title

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -6,9 +6,17 @@ RSpec.describe Page, type: :model do
   describe 'validations' do
     it { is_expected.to belong_to(:user)}
     it { is_expected.to be_valid }
-    it { is_expected.to validate_presence_of(:title)}
+    # it { is_expected.to validate_presence_of(:title)}
     it { is_expected.to validate_uniqueness_of(:title)}
     it { is_expected.to validate_presence_of(:content)}
+  end
+
+  describe '#slug' do
+    let(:page) { create(:page, title: '--Foo Bar! _ 87 --') }
+
+    it 'is generated from the title' do
+      expect(page.slug).to eq('foo-bar-87')
+    end
   end
 
   let(:page) { build(:page) }
