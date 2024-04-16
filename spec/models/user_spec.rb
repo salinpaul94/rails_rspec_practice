87f17aedae12d9fb4185f12ce5954f6ceb126d3f
salinpaul valooran <salin.paul@geckolyst.com>
2024-04-16 23:51:10 +0530

    cleaning up user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  subject { build(:user) }
 
   describe 'validations' do
+    it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value('foo@bar.com').for(:email) }
     it { is_expected.to_not allow_value('foo@').for(:email)}
     it { is_expected.to_not allow_value('@bar.com').for(:email)}
-  end
-
-  it 'has a valid factory' do
-    expect(user).to be_valid 
-    # expect(user.valid?).to eq(true)
-    # expect('').to be_empty
-    # expect({a: 1}).to be_a(Hash)
   end
 end
