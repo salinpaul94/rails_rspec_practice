b5cd2754c7d27b596d22a22bb5263d53c3391253
salinpaul valooran <salin.paul@geckolyst.com>
2024-04-16 15:04:56 +0530

    defining subject at the start of spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  let(:page) { build(:page) }
+  subject { build(:page) }
+  # let(:page) { build(:page) }
+
+  describe 'validations' do
+    it { is_expected.to belong_to(:user)}
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:title)}
+    it { is_expected.to validate_uniqueness_of(:title)}
+    it { is_expected.to validate_presence_of(:content)}
+  end
 
   it 'has a valid factory' do
     expect(page).to be_valid
