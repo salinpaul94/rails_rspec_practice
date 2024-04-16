673eac97072ec9a0cd72aa46ec4e1c9a2616a88d
salinpaul valooran <salin.paul@geckolyst.com>
2024-04-16 16:28:35 +0530

    adding model validation to page model

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
   subject { build(:page) }
-  # let(:page) { build(:page) }
 
   describe 'validations' do
     it { is_expected.to belong_to(:user)}
@@ -11,6 +10,8 @@ RSpec.describe Page, type: :model do
     it { is_expected.to validate_uniqueness_of(:title)}
     it { is_expected.to validate_presence_of(:content)}
   end
+
+  let(:page) { build(:page) }
 
   it 'has a valid factory' do
     expect(page).to be_valid
